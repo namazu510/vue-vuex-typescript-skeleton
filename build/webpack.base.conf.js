188f4d260f48aff489d153580d8ef36eaa9ae17a
namaz <namazu510@hotmail.co.jp>
2017-01-10 22:16:44 +0900

    Update　単体でサーブまでできるように　npm startでexpressでサーブ

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -15,7 +15,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, '../dist'),
-    publicPath: '/dist/',
+    publicPath: '/',
     filename: '[name].[hash].js'
   },
   resolve: {
